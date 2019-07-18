b437b66d1c6a7fdf7a04f77406c9ee4658dc7781
boolfunction <yelesh@seznam.cz>
2019-07-18 23:37:32 +0200

    added argparse to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ import Logger, { LogLevel } from './logger';
 import NudeBot from './NudeBot';
 
 require('dotenv').config();
+const { ArgumentParser } = require('argparse');
 
-Logger.init(LogLevel.DEBUG);
+const parser = new ArgumentParser({ 
+    version: require('../package.json').version,
+    addHelp: true,
+    description: 'Parser for logger\'s level'
+});
+
+parser.addArgument([ '-ll', '--logLevel' ],  // expected arguments
+    {   
+        defaultValue: 1,
+        help: "type: \"-ll\" or \"--logLevel\" " +
+        "then type one of the keys: 1=ERROR 2=WARN 3=INFO 4=DEBUG. Default value is 1=ERROR."
+    }
+);
+const args = parser.parseArgs(); // gonna give us arguments object
+
+Logger.init(args.logLevel);
 const nudeBot = new NudeBot();
 nudeBot.commands();
 nudeBot.launch();
